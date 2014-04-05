e026530731ed514c20c4dfeb3a52e6b3ea27ff30
Justin Martin <frozenfire@thefrozenfire.com>
2014-04-05 14:24:25 -0700

    Moved HTTP into its own repository.

---
+++ Module.sql
@@ -0,0 +1,9 @@
+delimiter ;
+source vendor/HTTP/src/schema/Request.sql
+source vendor/HTTP/src/schema/Response.sql
+
+delimiter |
+source vendor/HTTP/src/procedures/Request.sql
+source vendor/HTTP/src/procedures/Response.sql
+
+delimiter ;
